e355d43c2b39947675d0fc1889d7069868a53533
lihong <lihong_sail@163.com>
2017-08-30 19:23:43 +0800

    commit

--- app/src/main/java/com/lee/asher/kotlinio/base/BaseActivity.kt
+++ app/src/main/java/com/lee/asher/kotlinio/base/BaseActivity.kt
@@ -2,7 +2,6 @@ package com.lee.asher.kotlinio.base
 
 import android.app.Activity
 import android.os.Bundle
-import com.lee.asher.kotlinio.extension.applyScheduleres
 
 /**
  * Created by lihong on 2017/8/21.
